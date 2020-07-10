106a29377d8c6ae35839dda20c21096d65a670a8
NotoriousSlinky <nkayyarath@gmail.com>
2020-07-10 01:06:29 -0400

    finished but img not displaying on refresh

--- Web Development/Dicee Challenge - Starting Files/index.js
+++ Web Development/Dicee Challenge - Starting Files/index.js
@@ -1,8 +1,26 @@
-
+//function generates random number from 1-6
 function randomNum1() {
     var random = Math.floor(Math.random() * 6) + 1;
     return random;
 }
 
-document.querySelector(".img1").setAttribute("img1", + "dice" + randomNum1() + ".png");
-document.querySelector(".img2").setAttribute("img2", + "dice" + randomNum1() + ".png");
+//initialize variables for dice image and link them to actual .png files
+var diceImg1 = "images/dice" + randomNum1() + ".png";
+var diceImg2 = "images/dice" + randomNum1() + ".png";
+
+//select query based on classes and set their attribute based on random generated image number
+document.querySelector(".img1").setAttribute("src", + diceImg1);
+document.querySelector(".img2").setAttribute("src", + diceImg2);
+
+//generates random numbers for players
+var player1 = randomNum1();
+var player2 = randomNum1();
+
+//compares players dice rolls
+if (player1 > player2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (player1 < player2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+} else {
+    document.querySelector("h1").innerHTML = "DRAW!";
+}
